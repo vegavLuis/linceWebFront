471a0d327fa4b5c01f767e8857473da6e9ea0444
Luis Vega <70670285+vegavLuis@users.noreply.github.com>
2023-11-16 00:42:05 -0600

    se agrega ruta 404

---
+++ src/views/Error.vue
@@ -0,0 +1,10 @@
+import
+<template>
+  <h1 class="text-center pt-12">404</h1>
+  <h2 class="text-center pb-12">Pagina no encontrada</h2>
+  <div class="d-flex justify-center">
+    <router-link to="/">
+      <v-btn>Regresar</v-btn>
+    </router-link>
+  </div>
+</template>
