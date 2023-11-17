241f16c43a19f050c7036a546a721d8f27cec155
Luis Vega <70670285+vegavLuis@users.noreply.github.com>
2023-11-16 22:26:44 -0600

    se crea vista quienes somos

---
+++ src/stores/quienesSomos/app.js
@@ -0,0 +1,38 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useAppQuienesSomos = defineStore('appQuienesSomos', () => {
+  const data = ref([
+    {
+      titulo: 'Historia',
+      texto: `Sillas Lince nace hace más de 30 años a partir de la necesidad de un joven deportista de alto rendimiento, en busca de una silla de ruedas funcional y ligera que le ayude a desarrollar sus habilidades deportivas y mejore su movilidad en su vida diaria, al no poder encontrar una opción se dio a la tarea de desarrollar el mismo sus propias sillas, con el tiempo y buscando siempre la mejor forma de innovar, los modelos fueron cambiando,  así como los materiales que al día de hoy destacan a sillas lince como la única empresa en Latino América en fabricar sillas de ruedas personalizadas deportivas y de uso cotidiano en aluminio aeronáutico.
+        Diseñadas por personas con discapacidad para personas con discapacidad 
+        `
+    },
+    {
+      titulo: 'Misión',
+      texto: `Nuestra misión es fabricar un producto que sea personalizado para cada individuo en su estilo de vida que necesite.
+
+      Nosotros pugnamos para proveer una ligera e individual silla, como si fuera un sastre, con el objetivo de que el producto permita el uso final a la vida que quieren, en vez de que la silla los limite.
+      
+      Nosotros no somos sólo una fabrica de sillas, construimos sillas activas, para personas con discapacidad activa.
+      
+      Ligeras y hechas a mano en México, ROE-MEX sillas de ruedas deportivas y activas es una de la elecciones por muchos de los mejores atletas de alto rendimiento del mundo!!! Ya sea esta en la cancha o de día a día.
+      
+      Productos: Fabricamos de ultra ligera manuales como: La serie Lince, SKY, Akiva, y Shockwave (estructura en L). Las deportivas B-Ball como: Impacto, Challenger (ajustable), y 2EZ. Toda la serie de sillas LINCE es medida y fabricada por nuestros expertos conocedores en el campo.
+          `
+    },
+    {
+      titulo: 'Visión',
+      texto: `La visión fue crear una silla de ruedas que permita el desarrollo y que no se viera como ninguna otra silla en el mercado en ese tiempo. Después de 2 años la pasión y la ambición cobro vida y mas fuerza que nunca! Y se formo ROE-MEX
+
+        ROE-MEX tiene el mismo y simple acercamiento que hizo por mas de 25 años atrás y continua innovando y creando un producto que sostiene el abuso de algunos de los mejores atletas y permitiendo que se exhiba un hermoso arte entre el control de la silla y la ruda habilidad atlética.
+        
+        Nosotros continuaremos empujado e innovando productos que permitan que cualquiera que presente una discapacidad y la correcta actitud haga que sus metas se lleven acabo. 
+            `
+    }
+  ])
+  return {
+    data
+  }
+})
---
+++ src/views/QuienesSomos.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useAppQuienesSomos } from '../stores/quienesSomos/app'
+const datos = ref([])
+
+onMounted(() => {
+  datos.value = useAppQuienesSomos()
+})
+</script>
+<template>
+  <v-container>
+    <h1 class="text-center titulo pt-6 pb-4">Industrias Lince por Roe-Mex SA de CV</h1>
+    <v-divider :thickness="2" color="black" class="border-opacity-100"></v-divider>
+
+    <v-card v-for="item in datos.data" :key="item.titulo" elevation="0" class="pt-6 pb-6">
+      <h2 class="titulo2 pt-4 pb-4">{{ item.titulo }}</h2>
+      <v-divider :thickness="1" color="black" class="border-opacity-100"></v-divider>
+      <p class="pt-4 parrafo">{{ item.texto }}</p>
+    </v-card>
+  </v-container>
+</template>
+<style scoped>
+.titulo {
+  color: #8b0000;
+}
+.titulo2 {
+  color: #490036;
+}
+.parrafo {
+  line-height: 2.2;
+}
+</style>
