a1ca5d8f3b05f5e52ab2a7fee5cbd3d8e6ba6118
Luis Vega <70670285+vegavLuis@users.noreply.github.com>
2023-11-21 22:56:45 -0600

    se agrega Dynamic

--- src/stores/usodiario/sillasUsodiario.js
+++ src/stores/usodiario/sillasUsodiario.js
@@ -31,6 +31,18 @@ import ImagenSky1 from '../../assets/sillasUsoDiario/sky/img1.png'
 import ImagenSky2 from '../../assets/sillasUsoDiario/sky/img2.png'
 import ImagenSky3 from '../../assets/sillasUsoDiario/sky/img3.png'
 import ImagenSky4 from '../../assets/sillasUsoDiario/sky/img4.png'
+import ImagenDynamic1 from '../../assets/sillasUsoDiario/dynamic/img1.png'
+import ImagenDynamic2 from '../../assets/sillasUsoDiario/dynamic/img2.png'
+import ImagenDynamic3 from '../../assets/sillasUsoDiario/dynamic/img3.png'
+import ImagenDynamic4 from '../../assets/sillasUsoDiario/dynamic/img4.png'
+import ImagenDynamic5 from '../../assets/sillasUsoDiario/dynamic/img5.png'
+import ImagenDynamic6 from '../../assets/sillasUsoDiario/dynamic/img6.png'
+import ImagenDynamic7 from '../../assets/sillasUsoDiario/dynamic/img7.png'
+import ImagenDynamic8 from '../../assets/sillasUsoDiario/dynamic/img8.png'
+import ImagenDynamic9 from '../../assets/sillasUsoDiario/dynamic/img9.png'
+import ImagenDynamic10 from '../../assets/sillasUsoDiario/dynamic/img10.png'
+import ImagenDynamic11 from '../../assets/sillasUsoDiario/dynamic/img11.png'
+import ImagenDynamic12 from '../../assets/sillasUsoDiario/dynamic/img12.png'
 
 export const useSillasUsoDiario = defineStore('sillasUsoDiario', () => {
   const data = ref([
@@ -202,6 +214,48 @@ export const useSillasUsoDiario = defineStore('sillasUsoDiario', () => {
         { src: ImagenSky2 },
         { src: ImagenSky3 },
         { src: ImagenSky4 }
+      ],
+      info: `* Las imágenes son ilustrativas, el tipo de rodamientos traseros, frenos, impulsores, coderas, etc., tienen costo extra por lo que deberán ser cotizadas.`
+    },
+    {
+      id: 6,
+      nombre: 'Lince Dynamic',
+      descripcionPrevia: `Luce espectacular con nuevo y elegante diseño. Una de las más ágiles y ligeras del mercado, tan versátil que es ideal para disfrutar de un agradable paseo cotidiano o para practicar danza deportiva.`,
+      especificaciones: [
+        {
+          descripcion: `Construida con tubo de 1 1/2 pulg. parte inferior y 1 pulg. superior (asiento) de aluminio aeronáutico 6061-T6.`
+        },
+        { descripcion: `Capacidad de carga 110 kg.` },
+        { descripcion: `Respaldo fijo y ajustable en altura.` },
+        { descripcion: `Protectores de ropa fijos soldados a la estructura` },
+        { descripcion: `Reposapiés en “U” con ajuste de altura.` },
+        { descripcion: `Rodamientos traseros de 24" o 25" pulg.` },
+        { descripcion: `Centro de gravedad fijo` },
+        {
+          descripcion: `Vestidura acojinada repelente al agua, cojín de 2 pulgadas. espuma de alta densidad`
+        },
+        { descripcion: `Rodamiento delantero de 4 o 5 pulg. rin de plástico.` },
+        {
+          descripcion: `Fabricada acorde a las características fisiometricas y tipo de lesión de la persona. (personalizada)`
+        },
+        {
+          descripcion: `Aplicación de pintura Powder Coating, colores a elección de la Gama de Colores Roe-Mex.`
+        },
+        { descripcion: `Peso de estructura desde 6 kg. sin rodamientos ni accesorios` }
+      ],
+      imagenes: [
+        { src: ImagenDynamic1 },
+        { src: ImagenDynamic2 },
+        { src: ImagenDynamic3 },
+        { src: ImagenDynamic4 },
+        { src: ImagenDynamic5 },
+        { src: ImagenDynamic6 },
+        { src: ImagenDynamic7 },
+        { src: ImagenDynamic8 },
+        { src: ImagenDynamic9 },
+        { src: ImagenDynamic10 },
+        { src: ImagenDynamic11 },
+        { src: ImagenDynamic12 }
       ],
       info: `* Las imágenes son ilustrativas, el tipo de rodamientos traseros, frenos, impulsores, coderas, etc., tienen costo extra por lo que deberán ser cotizadas.`
     }
