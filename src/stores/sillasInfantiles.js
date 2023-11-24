b7de2e2791699ad1fead251334f489f7ed57eb01
Luis Vega <70670285+vegavLuis@users.noreply.github.com>
2023-11-24 13:33:46 -0600

    se agrega PC Infantil

--- src/stores/sillasInfantiles.js
+++ src/stores/sillasInfantiles.js
@@ -4,6 +4,11 @@ import imagenNitroInfantil1 from '../assets/sillasInfantiles/nitroInfantil/img1.
 import imagenNitroInfantil2 from '../assets/sillasInfantiles/nitroInfantil/img2.png'
 import imagenNitroInfantil3 from '../assets/sillasInfantiles/nitroInfantil/img3.png'
 import imagenNitroInfantil4 from '../assets/sillasInfantiles/nitroInfantil/img4.png'
+import imagenPcInfantil1 from '../assets/sillasInfantiles/pcInfantil/img1.png'
+import imagenPcInfantil2 from '../assets/sillasInfantiles/pcInfantil/img2.png'
+import imagenPcInfantil3 from '../assets/sillasInfantiles/pcInfantil/img3.png'
+import imagenPcInfantil4 from '../assets/sillasInfantiles/pcInfantil/img4.png'
+import imagenPcInfantil5 from '../assets/sillasInfantiles/pcInfantil/img5.png'
 
 export const useSillasInfantiles = defineStore('sillasInfantiles', () => {
   const data = ref([
@@ -35,6 +40,28 @@ export const useSillasInfantiles = defineStore('sillasInfantiles', () => {
         { src: imagenNitroInfantil4 }
       ],
       info: `*Las imagenes son ilustrativas, el tipo de rodamiento trasero así como frenos, impulsores, coderas, etc, tienen costo extra por lo que deberan ser cotizadas`
+    },
+    {
+      id: 2,
+      nombre: `Lince Nitro Infantil`,
+      descripcionPrevia: `Especial para niños diagnosticados con Parálisis Cerebral`,
+      especificaciones: [
+        { descripcion: `Respaldo acojinado.` },
+        { descripcion: `Banda para pantorrillas.` },
+        { descripcion: `Respaldo plegable.` },
+        { descripcion: `Estructura de aluminio aeronáutico 6061 T-6.` },
+        {
+          descripcion: `Aplicación de pintura Powder Coating, colores a elección de la Gama de Colores Roe-Mex.`
+        }
+      ],
+      imagenes: [
+        { src: imagenPcInfantil1 },
+        { src: imagenPcInfantil2 },
+        { src: imagenPcInfantil3 },
+        { src: imagenPcInfantil4 },
+        { src: imagenPcInfantil5 }
+      ],
+      info: `*Las imágenes son ilustrativas, el tipo de rodamientos traseros así como frenos, impulsores, coderas, etc., tienen costo extra por lo que deberán ser cotizadas. Los frenos , impulsores, coderas o tipo rodamientos tienen costo extra`
     }
   ])
   return {
