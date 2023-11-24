d0346490ca3c51ecc4fd4089fd2ba24c8f8258ec
Luis Vega <70670285+vegavLuis@users.noreply.github.com>
2023-11-24 13:55:59 -0600

    se agrega akiva juvenil

--- src/stores/sillasInfantiles.js
+++ src/stores/sillasInfantiles.js
@@ -9,6 +9,11 @@ import imagenPcInfantil2 from '../assets/sillasInfantiles/pcInfantil/img2.png'
 import imagenPcInfantil3 from '../assets/sillasInfantiles/pcInfantil/img3.png'
 import imagenPcInfantil4 from '../assets/sillasInfantiles/pcInfantil/img4.png'
 import imagenPcInfantil5 from '../assets/sillasInfantiles/pcInfantil/img5.png'
+import imagenAkivaJuvenil1 from '../assets/sillasInfantiles/akivaJuvenil/img1.png'
+import imagenAkivaJuvenil2 from '../assets/sillasInfantiles/akivaJuvenil/img2.png'
+import imagenAkivaJuvenil3 from '../assets/sillasInfantiles/akivaJuvenil/img3.png'
+import imagenAkivaJuvenil4 from '../assets/sillasInfantiles/akivaJuvenil/img4.png'
+import imagenAkivaJuvenil5 from '../assets/sillasInfantiles/akivaJuvenil/img5.png'
 
 export const useSillasInfantiles = defineStore('sillasInfantiles', () => {
   const data = ref([
@@ -62,6 +67,36 @@ export const useSillasInfantiles = defineStore('sillasInfantiles', () => {
         { src: imagenPcInfantil5 }
       ],
       info: `*Las imágenes son ilustrativas, el tipo de rodamientos traseros así como frenos, impulsores, coderas, etc., tienen costo extra por lo que deberán ser cotizadas. Los frenos , impulsores, coderas o tipo rodamientos tienen costo extra`
+    },
+    {
+      id: 3,
+      nombre: `Lince Akiva juvenil`,
+      descripcionPrevia: `Gran diseño y sencillez fabricada especialmente para pequeños. Lince Akiva Infantil se distingue por su marco tobusto, que la hace ligera, resistente y muy segura. Sin lugar a dudas una gran opcion para acompañar en la vida activa de un jovencito`,
+      especificaciones: [
+        { descripcion: `Construida con tubo de 1¼ de pulg. de aluminio aeronáutico 6061-T6` },
+        { descripcion: `Peso de 7 a 9 KG` },
+        { descripcion: `Respaldo plegable` },
+        { descripcion: `Modelo vanguardista en "L"` },
+        { descripcion: `Protectores de ropa desilizables` },
+        { descripcion: `Reposapiés en "U" con ajustable de altura` },
+        { descripcion: `Rodamientos traseros de 24 pulg.` },
+        { descripcion: `Vestidura acojinada repelente al agua` },
+        { descripcion: `Rodamiento delantero de 5 pulg. rin de plastico llanta transparente` },
+        {
+          descripcion: `Fabricada acorde a las características fidiometricas y tipo de lesión de la persona`
+        },
+        {
+          descripcion: `Aplicacion de pintura Powder Coating, colores a elección de la Gama de colores Roe-Mex`
+        }
+      ],
+      imagenes: [
+        { src: imagenAkivaJuvenil1 },
+        { src: imagenAkivaJuvenil2 },
+        { src: imagenAkivaJuvenil3 },
+        { src: imagenAkivaJuvenil4 },
+        { src: imagenAkivaJuvenil5 }
+      ],
+      info: `*Las imagenes son ilustrativas, el tipo de rodamientos traseros así como de frenos, impulsores coderas, etc, tienen costo extra por lo que deberán ser cotizadas. Los frenos, impulsores, coderas o tipo rodamientos tienen costo extra`
     }
   ])
   return {
