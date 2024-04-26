8321df416f7d8ffec81fef98084f90af719dbc22
Luis Vegea <vegalu63@gmail.com>
2024-04-25 19:15:48 -0600

    se cambia vista de redes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,14 +10,73 @@ const storeRedes = useRedesSociales()
 <template>
   <v-footer color="#020202" class="mt-12 text-center d-flex flex-column">
     <v-row justify="center" no-gutters>
-      <template v-for="item in storeRedes.data" :key="item">
+      <div class="d-flex flex-wrap justify-space-evenly redes">
+        <v-card color="transparent" elevation="0">
+          <a :href="storeRedes.data[0].to">
+            <v-icon
+              class="ma-2"
+              size="50px"
+              :color="storeRedes.data[0].color"
+              :icon="storeRedes.data[0].icon"
+            ></v-icon>
+          </a>
+          <a :href="storeRedes.data[5].to">
+            <v-icon
+              class="ma-2"
+              size="50px"
+              :color="storeRedes.data[5].color"
+              :icon="storeRedes.data[5].icon"
+            ></v-icon>
+          </a>
+          <p>Nuetras experiencias</p>
+        </v-card>
+        <v-card color="transparent" elevation="0">
+          <a :href="storeRedes.data[3].to">
+            <v-icon
+              class="ma-2"
+              size="50px"
+              :color="storeRedes.data[3].color"
+              :icon="storeRedes.data[3].icon"
+            ></v-icon>
+          </a>
+          <a :href="storeRedes.data[2].to">
+            <v-icon
+              class="ma-2"
+              size="50px"
+              :color="storeRedes.data[2].color"
+              :icon="storeRedes.data[2].icon"
+            ></v-icon>
+          </a>
+          <a :href="storeRedes.data[1].to">
+            <v-icon
+              class="ma-2"
+              size="50px"
+              :color="storeRedes.data[1].color"
+              :icon="storeRedes.data[1].icon"
+            ></v-icon>
+          </a>
+          <p>Contactanos</p>
+        </v-card>
+        <v-card color="transparent" elevation="0">
+          <a :href="storeRedes.data[4].to">
+            <v-icon
+              class="ma-2"
+              size="50px"
+              :color="storeRedes.data[4].color"
+              :icon="storeRedes.data[4].icon"
+            ></v-icon>
+          </a>
+          <p>Ubicacion</p>
+        </v-card>
+      </div>
+      <!-- <template v-for="item in storeRedes.data" :key="item">
         <router-link :to="item.to" style="text-decoration: none; color: inherit">
           <v-card class="ma-2" elevation="0" color="transparent">
             <v-icon class="ma-2" size="50px" :color="item.color" :icon="item.icon"> </v-icon>
             <p>{{ item.texto }}</p>
           </v-card>
         </router-link>
-      </template>
+      </template> -->
       <!--
       <v-col cols="12" class="text-center mt-6">
         <h6 class="text-center">DISTRIBUIDORES AUTORIZADOS</h6>
@@ -41,26 +100,27 @@ const storeRedes = useRedesSociales()
       <p class="small mr-4 pt-2">Tel: + (55) 5441 9219 / (55) 5732 9348</p>
       <p class="small mr-4 pt-2">WhatsApp: 5581020704</p>
       <a href="mailto:sillaslince@gmail.com" class="router">
-        <p class="small mr-4 pt-2">Quejas y sugerencias <v-icon class="mail">mdi-gmail</v-icon></p>
+        <!-- <v-icon class="mail">mdi-gmail</v-icon> -->
+        <p class="small mr-4 pt-2">Quejas y sugerencias</p>
       </a>
       <!-- <div class="d-flex flex-column">
       </div> -->
-      <v-btn color="transparent">
+      <!-- <v-btn color="transparent">
         <v-icon icon="mdi-map-marker" color="red"></v-icon>
-      </v-btn>
+      </v-btn> -->
       <!-- <v-btn color="transparent">
         <v-icon size="x-large" icon="mdi-facebook" color="#3b5998"></v-icon>
       </v-btn> -->
-      <v-btn color="transparent">
+      <!-- <v-btn color="transparent">
         <v-icon size="x-large" icon="mdi-instagram" color="#E1306C"></v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-col cols="12" class="text-center mt-6">
         <p class="text-center">DISTRIBUIDORES AUTORIZADOS</p>
       </v-col>
 
       <v-card
-        width="30px"
-        height="30px"
+        width="55px"
+        height="55px"
         elevation="0"
         class="mr-12"
         color="transparent"
@@ -82,6 +142,9 @@ const storeRedes = useRedesSociales()
 .router {
   text-decoration: none;
   color: white;
+}
+.redes {
+  width: 90vw;
 }
 .mail {
   color: #ef5350;
--- src/stores/redesSociales.js
+++ src/stores/redesSociales.js
@@ -9,12 +9,12 @@ export const useRedesSociales = defineStore('redesSociales', () => {
       color: '#3b5998',
       texto: 'Conoce nuestras experiencias'
     },
-    // {
-    //   icon: 'mdi-gmail',
-    //   to: 'mailto:sillaslince@gmail.com',
-    //   color: '#ef5350',
-    //   texto: 'Quejas y sugerencias'
-    // },
+    {
+      icon: 'mdi-gmail',
+      to: 'mailto:sillaslince@gmail.com',
+      color: '#ef5350',
+      texto: 'Quejas y sugerencias'
+    },
     {
       icon: 'mdi-whatsapp',
       to: 'https://api.whatsapp.com/send?phone=5215581020704',
@@ -26,6 +26,18 @@ export const useRedesSociales = defineStore('redesSociales', () => {
       to: 'tel:+525581020704',
       color: '#0D47A1',
       texto: 'Llamanos de 9:30 am a 17:30 pm'
+    },
+    {
+      icon: 'mdi-map-marker',
+      to: 'https://maps.app.goo.gl/suseWbmzdEBS4EdC6',
+      color: 'red',
+      texto: ''
+    },
+    {
+      icon: 'mdi-instagram',
+      to: 'https://www.instagram.com/sillas_lince/?hl=es',
+      color: 'red',
+      texto: ''
     }
   ])
   return {
