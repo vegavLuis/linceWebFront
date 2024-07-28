77b8d61ad4fca50951370bbd955f739ca32a8dae
Luis Vegea <vegalu63@gmail.com>
2024-07-28 15:35:41 -0600

    Reapply "Merge branch 'cambiosPrincipal' into dev"
    This reverts commit 58b5d707d0c6079428b7c3cbd2af28766bd1c149.

---
+++ src/components/promociones/Promo1.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PDF from 'pdf-vue3-vite'
+import promo from '../../assets/promociones/Promocion_PARIS_2024-Cojines_RoHo.pdf'
+</script>
+<template>
+  <div class="mt-4">
+    <h3>Promocion PARIS 2024 - Cojines RoHo</h3>
+    <PDF :src="promo" />
+  </div>
+</template>
+<style scoped></style>
