e19490c0f6f660c8678e1ccc474966026cdf5663
Luis Vegea <vegalu63@gmail.com>
2024-09-26 00:28:31 -0600

    se cambia

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://89.116.191.144/api/'
+  baseURL: 'http://89.116.191.144/api'
   // baseURL: 'http://localhost:3000/api/'
 })
 export default api
--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
 // const URL = 'http://localhost:3000'
-const URL = 'http://89.116.191.144/'
+const URL = 'http://89.116.191.144'
 
 export const socket = io(URL)
