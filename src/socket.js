7ed13a07af66bb16f2c336254c1d74eec6285960
Luis Vegea <vegalu63@gmail.com>
2024-09-26 00:23:48 -0600

    se cambia

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:3000'
-// const URL = "http://191.101.232.150/";
+// const URL = 'http://localhost:3000'
+const URL = 'http://89.116.191.144/'
 
 export const socket = io(URL)
