6fdf1268eea0e9a5813aaffc2742a9b0c7460582
Luis Vegea <vegalu63@gmail.com>
2024-10-09 00:11:54 -0600

    Se agrega socket

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:3000'
+const URL = import.meta.env.VITE_URL
 // const URL = 'http://89.116.191.144'
 
 export const socket = io(URL)
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,46 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
+// export default defineConfig({
+//   plugins: [vue(), vueJsx()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: JSON.stringify(env.VITE_URL),
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// })
+export default defineConfig(({ mode }) => {
+  // Cargar las variables de entorno según el modo
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-      // '/socket.io': {
-      //   target: 'http://89.116.191.144',
-      //   changeOrigin: true,
-      //   ws: true,
-      //   secure: false
-      // }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_URL, // Usa env directamente sin JSON.stringify
+          changeOrigin: true,
+          secure: false
+        }
+      }
     }
   }
 })
