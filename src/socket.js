17bdd59caf0132a1d02506b9884a949e64406043
Luis Vegea <vegalu63@gmail.com>
2024-11-01 00:52:56 -0600

    cambios

--- src/socket.js
+++ src/socket.js
@@ -2,15 +2,6 @@ import { io } from 'socket.io-client'
 
 //const URL = import.meta.env.VITE_URL
 // const URL = 'http://89.116.191.144'
-const URL = 'wss://localhost:3000'
+const URL = 'wss://app.roe-mex.com.mx'
 
 export const socket = io(URL)
-
-// Manejadores de eventos
-socket.on('connect', () => {
-  console.log('Conectado al servidor WebSocket')
-})
-
-socket.on('connect_error', (error) => {
-  console.error('Error de conexión:', error)
-})
