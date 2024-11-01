9044a60deda3e16746d58755299d03e98dd5bfa0
Luis Vegea <vegalu63@gmail.com>
2024-11-01 00:59:59 -0600

    cambios

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client'
 
 //const URL = import.meta.env.VITE_URL
 // const URL = 'http://89.116.191.144'
-const URL = 'wss://app.roe-mex'
+const URL = 'https://app.roe-mex.com.mx'
 
 export const socket = io(URL)
